5955dd484997e05050aa425a083a38afd70719f3
unknown <junbeom247@gmail.com>
2023-04-17 15:38:09 +0900

    fix : table 컬럼명과 일치시키기 위해 변수명 변경

--- src/main/java/crawling/Crawling.java
+++ src/main/java/crawling/Crawling.java
@@ -24,43 +24,44 @@ public class Crawling {
 		List<TeamVO> teams = new ArrayList<>();
 		List<LeagueVO> leagues = new ArrayList<>();
 		for (int i = 0; i < teamElements.size(); i++) {
-			String team = teamElements.get(i).select("h2 > a").get(1).text();
+			String teamName = teamElements.get(i).select("h2 > a").get(1).text();
 			Elements inPlayers = teamElements.get(i).select(".responsive-table").get(0).select("table > tbody > tr");
 			Elements outPlayers = teamElements.get(i).select(".responsive-table").get(0).select("table > tbody > tr");
 
 			// in
 			for (int j = 0; j < inPlayers.size(); j++) {
 				String idStr = inPlayers.get(j).select("td > .di > .show-for-small > a").get(0).attr("href");
-				String id = idStr.substring(idStr.lastIndexOf("/") + 1, idStr.length());
-				String name = inPlayers.get(j).select("td > .di > .show-for-small > a").get(0).text();
+				String player_id = idStr.substring(idStr.lastIndexOf("/") + 1, idStr.length());
+				String player_name = inPlayers.get(j).select("td > .di > .show-for-small > a").get(0).text();
 				String age = inPlayers.get(j).select("td").get(1).text();
 				String nation = inPlayers.get(j).select("td > img").get(0).attr("title");
 				String position = inPlayers.get(j).select("td").get(3).text();
-				String departTeam = inPlayers.get(j).select("td.zentriert > a > img").get(0).attr("title");
-				String departLeague = inPlayers.get(j).select("td.verein-flagge-transfer-cell > img").size() == 0 ? "-"
+				String previousTeam = inPlayers.get(j).select("td.zentriert > a > img").get(0).attr("title");
+				String previousLeague = inPlayers.get(j).select("td.verein-flagge-transfer-cell > img").size() == 0 ? "-"
 						: inPlayers.get(j).select("td.verein-flagge-transfer-cell > img").get(0).attr("title");
 				String fee = inPlayers.get(j).select("td.rechts > a").get(0).text();
 
 				PlayerVO player = new PlayerVO();
-				player.setPlayer_id(Integer.parseInt(id));
-				player.setPlayer_name(name);
+				player.setPlayer_id(Integer.parseInt(player_id));
+				player.setPlayer_name(player_name);
 				players.add(player);
 			}
 
+			// out
 			for (int j = 0; j < outPlayers.size(); j++) {
 				String outStr = outPlayers.get(j).select("td > .di > .show-for-small > a").get(0).attr("href");
-				String id = outStr.substring(outStr.lastIndexOf("/") + 1, outStr.length());
-				String name = outPlayers.get(j).select("td > .di > .show-for-small > a").get(0).text();
+				String player_id = outStr.substring(outStr.lastIndexOf("/") + 1, outStr.length());
+				String player_name = outPlayers.get(j).select("td > .di > .show-for-small > a").get(0).text();
 				String age = outPlayers.get(j).select("td").get(1).text();
 				String nation = outPlayers.get(j).select("td > img").get(0).attr("title");
 				String position = outPlayers.get(j).select("td").get(3).text();
-				String joinTeam = outPlayers.get(j).select("td.zentriert > a > img").get(0).attr("title");
-				String joinLeague = outPlayers.get(j).select("td.verein-flagge-transfer-cell > img").size() == 0 ? "-"
+				String newTeam = outPlayers.get(j).select("td.zentriert > a > img").get(0).attr("title");
+				String newLeague = outPlayers.get(j).select("td.verein-flagge-transfer-cell > img").size() == 0 ? "-"
 						: inPlayers.get(j).select("td.verein-flagge-transfer-cell > img").get(0).attr("title");
 				String fee = outPlayers.get(j).select("td.rechts > a").get(0).text();
 				PlayerVO player = new PlayerVO();
-				player.setPlayer_id(Integer.parseInt(id));
-				player.setPlayer_name(name);
+				player.setPlayer_id(Integer.parseInt(player_id));
+				player.setPlayer_name(player_name);
 				players.add(player);
 			}
 		}
