a989c3275aac3ab971b4628715db45299362b4ba
unknown <junbeom247@gmail.com>
2023-04-17 12:18:21 +0900

    docs : playerService 주석 작성

--- src/main/java/service/PlayerService.java
+++ src/main/java/service/PlayerService.java
@@ -6,10 +6,22 @@ import vo.PlayerVO;
 public class PlayerService {
 	
 	PlayerDAO playerDao = new PlayerDAO();
+	
+	/**
+	 * PlayerVO를 받아 저장하는 기능
+	 * @param player
+	 * @return 저장 성공 시 1, 실패시 0
+	 */
 	public int insertPlayer(PlayerVO player) {
 		return playerDao.insertPlayer(player);
 	}
 
+	/**
+	 * *****Null return 가능*****
+	 * player_id를 받아 아이디에 해당하는 PlayerVO 리턴 
+	 * @param player_id
+	 * @return 해당하는 아이디가 있으면 PlayerVO, 없으면 null return
+	 */
 	public PlayerVO selectPlayerById(int player_id) {
 		return playerDao.selectPlayerById(player_id);
 	}
