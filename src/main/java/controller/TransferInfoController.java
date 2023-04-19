87ae805e98b524de79836af44e623b953900879b
unknown <seojh0609@naver.com>
2023-04-19 13:38:29 +0900

    feat : 해당리그의 이적기록을 조회하는 컨트롤러 구현

--- src/main/java/controller/TransferInfoController.java
+++ src/main/java/controller/TransferInfoController.java
@@ -1,13 +1,27 @@
 package controller;
 
 import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import service.TransferService;
+import vo.TransferVO;
 
-public class TransferInfoController implements TranferControllerInterface{
+/**
+ * 해당리그 이적 조회 컨트롤러
+ * TransferService에 leagueName을 매개변수로 하고 연도,시즌 조건 만족하는 selectTransferAll 메서드 필요
+ * @param leagueName(리그이름)
+ * @return transferInfo.jsp로 forward
+ * 작성자 : 서준호
+ */
+public class TransferInfoController implements Controller {
 
 	@Override
 	public String execute(Map<String, Object> data) throws Exception {
-		
-		return null;
+		HttpServletRequest request = (HttpServletRequest) data.get("request");
+		TransferService service = new TransferService();
+		String leagueName = request.getParameter("leagueName");		
+		TransferVO transfer = service.selectTransferAll(leagueName);
+		request.setAttribute("transferInfo", transfer);
+				
+		return "transferInfo.jsp";
 	}
-
 }
