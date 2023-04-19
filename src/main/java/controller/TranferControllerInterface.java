2ccaa2c5f9560362fcbe4103daa871ecc6948753
unknown <seojh0609@naver.com>
2023-04-19 09:58:18 +0900

    feat : controller 기본 틀 생성

---
+++ src/main/java/controller/TranferControllerInterface.java
@@ -0,0 +1,8 @@
+package controller;
+
+import java.util.Map;
+
+public interface TranferControllerInterface {
+
+	public String execute(Map<String, Object> data) throws Exception;
+}
---
+++ src/main/java/controller/TransferInfoController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.util.Map;
+
+public class TransferInfoController implements TranferControllerInterface{
+
+	@Override
+	public String execute(Map<String, Object> data) throws Exception {
+		
+		return null;
+	}
+
+}
