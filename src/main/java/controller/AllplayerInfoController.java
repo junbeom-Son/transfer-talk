ffee83e318e7c192bf9213df42694dd139efd074
unknown <seojh0609@naver.com>
2023-04-27 16:44:29 +0900

    feat : 리턴 코드 수정

--- src/main/java/controller/AllplayerInfoController.java
+++ src/main/java/controller/AllplayerInfoController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import service.PlayerService;
 import vo.PlayerVO;
 
@@ -23,8 +25,7 @@ public class AllplayerInfoController implements Controller {
 		PlayerService playerService = new PlayerService();
 		String playerName = request.getParameter("playerName");		
 		List<PlayerVO> players = playerService.selectPlayersByName(playerName);
-		request.setAttribute("PlayerAll", players);
-				
-		return "PlayerAll.jsp";
+		ObjectMapper objectMapper = new ObjectMapper();
+		return "responseBody:" + objectMapper.writeValueAsString(players);
 	}
 }
